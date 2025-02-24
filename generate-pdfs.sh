7b76edc9e0fd376860fa5da41906e606550e6f3f
nimblestart <nimble.start@gmail.com>
2025-02-24 09:33:14 -0500

    Update generate-pdfs.sh

--- generate-pdfs.sh
+++ generate-pdfs.sh
@@ -33,6 +33,9 @@ find "$REPO_DIR" -mindepth 1 -maxdepth 1 -type d | while read -r SUBJECT_DIR; do
     # Format subject name for title (e.g., "ap-biology" -> "AP Biology")
     CLEAN_SUBJECT_NAME=$(echo "$SUBJECT" | tr '-' ' ' | awk '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2))}1')
 
+    # Special case for "AP"
+    CLEAN_SUBJECT_NAME=$(echo "$CLEAN_SUBJECT_NAME" | sed 's/\bAp\b/AP/g')
+
     echo "Formatted Subject Name: $CLEAN_SUBJECT_NAME"
 
     # Gather all unit notes (exclude README.md and resources.md)
